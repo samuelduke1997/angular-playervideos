04c899664e8db85d5f25f07761879d3ac6d2c7ed
samuelduke1997 <samuelduquedev@gmail.com>
2021-10-28 11:06:33 -0300

    feat: add page login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AutenticacaoGuard } from './autenticacao/autenticacao.guard';
+import { LoginGuard } from './autenticacao/login.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+    canLoad: [LoginGuard],
+  },
+  {
+    path: 'pagina-inicial',
+    loadChildren: () =>
+      import('./pagina-inicial/pagina-inicial.module').then(
+        (m) => m.PaginaInicialModule
+      ),
+    canLoad: [LoginGuard],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
